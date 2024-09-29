b17d45ba093bc1318c34bed6a76d20850419a494
Riley Mei <rileymmei@gmail.com>
2024-09-29 02:18:53 -0600

    pages can now be rendered using ejs

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express'); // To build an application server or API
 const app = express();
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
 
 app.get('/home', (req, res) => {
-    res.render('pages/home.html');
+    res.render('home.html');
 });
 
 app.get('/input', (req, res) => {
-    res.render('pages/input.html');
+    res.render('input.html');
 });
 
 app.get('/result', (req, res) => {
-    res.render('pages/result.html');
+    res.render('result.html');
 });
 
 app.get('/about', (req, res) => {
-    res.render('pages/about.html');
+    res.render('about.html');
 });
 
 app.listen(3000);
