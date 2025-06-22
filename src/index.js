436c19fc45dd0bfc51c6486009b1d2b8ea795910
riley mei <rileymei@rileys-MacBook-Pro.local>
2025-06-21 21:58:37 -0700

    no tls maybe work?

--- src/index.js
+++ src/index.js
@@ -16,7 +16,13 @@ const app = express();
 app.use(express.json());
 
 // Set up Redis client for session store
-const redisClient = new Redis('rediss://default:arSj2UkaJ7BZm6t3gHPln3xsUYsuFIOW@redis-16874.c241.us-east-1-4.ec2.redns.redis-cloud.com:16874');
+const redisClient = new Redis({
+  host: 'redis-16874.c241.us-east-1-4.ec2.redns.redis-cloud.com',
+  port: 16874,
+  username: 'default',
+  password: 'arSj2UkaJ7BZm6t3gHPln3xsUYsuFIOW'
+  // No TLS
+});
 
 // Set up RedisStore instance for session store
 const store = new RedisStore({ client: redisClient });
